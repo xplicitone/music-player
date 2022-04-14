6b7c87a1e0cde8c07ad2467a469cb3eac0cb4c29
xplicitone <paul.kalaw@gmail.com>
2022-04-13 22:57:41 -0500

    Artist Data and List UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,52 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
 import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
+import { Image } from "@chakra-ui/react";
 
-const Home = () => {
+const Home = ({ artists }) => {
   return (
     <GradientLayout
       color="blue"
       roundImage
       subtitle="profile"
-      title="Paul K"
+      title="XplicitOne"
       description="11 public playlists"
+      image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
     >
-      <div>homepage</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artists this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
