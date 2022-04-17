666a1f59a9925e712df71ed370efc1008e9609c9
xplicitone <paul.kalaw@gmail.com>
2022-04-16 23:21:32 -0500

    Playlist Page UI updated

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,8 +1,27 @@
+import GradientLayout from "../../components/gradientLayout";
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
 
+const getBGColor = (id) => {
+  const colors = ["red", "green", "blue", "orange", "gray", "teal", "yellow"];
+
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
+
 const Playlist = ({ playlist }) => {
-  return <div>{playlist.name}</div>;
+  const color = getBGColor(playlist.id);
+  return (
+    <GradientLayout
+      color={color}
+      roundImage={false}
+      title={playlist.name}
+      subtitle="playlist"
+      description={`${playlist.songs.length} songs`}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+    >
+      <div>kajdfl</div>
+    </GradientLayout>
+  );
 };
 
 export const getServerSideProps = async ({ query, req }) => {
