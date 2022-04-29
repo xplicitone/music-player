d2ca944fd739c48785bdf7d36a089e1c45980149
xplicitone <paul.kalaw@gmail.com>
2022-04-29 00:30:58 -0500

    Shuffle, seek, end and repeat handlers

--- components/player.tsx
+++ components/player.tsx
@@ -27,13 +27,42 @@ import { formatTime } from "../lib/formatters";
 
 const Player = ({ songs, activeSong }) => {
   const [playing, setPlaying] = useState(true);
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(
+    songs.findIndex((s) => s.id === activeSong.id)
+  );
   const [seek, setSeek] = useState(0.0);
   const [isSeeking, setIsSeeking] = useState(false);
   const [repeat, setRepeat] = useState(false);
   const [shuffle, setShuffle] = useState(false);
   const [duration, setDuration] = useState(0.0);
   const soundRef = useRef(null);
+  const repeatRef = useRef(repeat);
+  const setActiveSong = useStoreActions((state: any) => state.changeActiveSong);
+
+  useEffect(() => {
+    let timerId;
+
+    if (playing && !isSeeking) {
+      const f = () => {
+        setSeek(soundRef.current.seek());
+        timerId = requestAnimationFrame(f); // recursive
+      };
+
+      timerId = requestAnimationFrame(f); // this will kick it off
+      return () => cancelAnimationFrame(timerId); // when returned inside useEffect, this f(x) gets called when the component that's using useEffect gets unmounted from the DOM. Clean up request animation frame (WebSocket or set interval, this is where you clean it up)
+    }
+
+    // if condition was false, do none of that and cancel the animationFrame as well with the timerId
+    cancelAnimationFrame(timerId);
+  }, [playing, isSeeking]);
+
+  useEffect(() => {
+    setActiveSong(songs[index]);
+  }, [index, setActiveSong, songs]);
+
+  useEffect(() => {
+    repeatRef.current = repeat;
+  }, [repeat]);
 
   const setPlayState = (value) => {
     setPlaying(value);
@@ -67,7 +96,7 @@ const Player = ({ songs, activeSong }) => {
   };
 
   const onEnd = () => {
-    if (repeat) {
+    if (repeatRef.current) {
       setSeek(0); // reset our UI
       soundRef.current.seek(0); // move the song back to 0
     } else {
@@ -160,7 +189,7 @@ const Player = ({ songs, activeSong }) => {
       <Box color="gray.600">
         <Flex justify="center" align="center">
           <Box width="10%">
-            <Text fontSize="xs">1:21</Text>
+            <Text fontSize="xs">{formatTime(seek)}</Text>
           </Box>
           <Box width="80%">
             <RangeSlider
